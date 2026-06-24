81d484046da0b6c812d7a974a492dbe8ed6fabe9
arthurcamargo03 <arthurcamargo561@gmail.com>
2026-06-24 14:27:44 -0300

    Forca saida em UTF-8 no Main para exibir acentos e setas corretamente

--- src/com/treinotracker/Main.java
+++ src/com/treinotracker/Main.java
@@ -2,8 +2,12 @@ package com.treinotracker;
 
 import com.treinotracker.ui.ConsoleApp;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
         new ConsoleApp().run();
     }
 }
